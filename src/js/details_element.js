e5a80b86d381d7a3ef0a0cd5ce1a59a548153073
vladimirovicp <vladimirovicp70@gmail.com>
2023-03-07 11:36:38 +0400

    refactor

--- src/js/details_element.js
+++ src/js/details_element.js
@@ -114,10 +114,53 @@ class Details1 {
 
 let details1 = document.querySelectorAll('.details1');
 
-if( details1.length > 0){
-    for(let index = 0; index < details1.length; index++){
-        details1[index].querySelectorAll('details').forEach((el) => {
-            new Details1(el);
-         });
+// if( details1.length > 0){
+//     for(let index = 0; index < details1.length; index++){
+//         details1[index].querySelectorAll('details').forEach((el) => {
+//             new Details1(el);
+//          });
+//     }
+// }
+
+
+let details2 = document.querySelector('.details2');
+
+if( details2){
+
+    // for(let index = 0; index < details2.length; index++){
+    //     details2[index].querySelectorAll('details').forEach((el) => {
+    //         new Details1(el);
+    //     });
+    // }
+
+    const details = details2.querySelectorAll(".item > details");
+    const detailsSub = details2.querySelectorAll(".item_sub > details");
+
+    for(i=0;i<details.length;i++) {
+        details[i].addEventListener("toggle", accordion);
+    }
+    for(let i=0;i<detailsSub.length;i++) {
+        detailsSub[i].addEventListener("toggle", accordion);
+    }
+    function accordion(event) {
+        if (!event.target.open) return;
+        let item = event.target.parentNode;
+        // console.log(event.target.children)
+        if( item.className === 'item'){
+            for(i=0;i<details.length;i++) {
+                if (details[i].tagName != "DETAILS" || !details[i].hasAttribute('open') || event.target == details[i]) continue;
+                details[i].removeAttribute("open");
+            }
+            for(i=0;i<detailsSub.length;i++) {
+                detailsSub[i].removeAttribute("open");
+            }
+        } else if( item.className === 'item_sub'){
+
+            const current = event.target.parentNode.children;
+            for(i=0;i<current.length;i++) {
+                if (current[i].tagName != "DETAILS" || !current[i].hasAttribute('open') || event.target == current[i]) continue;
+                current[i].removeAttribute("open");
+            }
+        }
     }
 }
