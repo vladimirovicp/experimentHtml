0cc475a8fa81d3cdca85b96eb1ab88dfe5f456db
vladimirovicp <vladimirovicp70@gmail.com>
2023-05-17 16:01:24 +0400

    add: rutube

--- src/js/rutube_video.js
+++ src/js/rutube_video.js
@@ -12,10 +12,15 @@ const videoRutubeGenerate = (video) => {
 
     const btn = video.querySelector('.video-block__button');
     const videoId = btn.dataset.videoId;
+    const container = video.querySelector('.video-block__inner');
 
-    //console.log(videoId);
+    console.log(videoId)
 
-    apiRutube(videoId)
+    btn.addEventListener('click',() => {
+        const iframe = iframeRutubeGenerate(videoId);
+        container.innerHTML = '';
+        container.appendChild(iframe)
+    })
 
 
 
@@ -32,37 +37,25 @@ const videoRutubeGenerate = (video) => {
     // })
 }
 
+const iframeRutubeGenerate = (videoId) => {
+    const iframe = document.createElement('iframe');
+    const src = "https://rutube.ru/play/embed/" + videoId;
+
+    iframe.setAttribute('src', src)
+    iframe.setAttribute('frameborder', '0')
+    iframe.setAttribute('allow', 'clipboard-write; autoplay')
+    iframe.setAttribute('webkitAllowFullScreen', '')
+    iframe.setAttribute('mozallowfullscreen', '')
+    iframe.setAttribute('allowfullscreen', '')
+
+    iframe.classList.add('video-block__content');
 
 
-async function apiRutube(id) {
+    //<iframe width="720" height="405" src="https://rutube.ru/play/embed/5cf99794db543cd3a49a6b5e4a0ebb5a" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 
+    console.log(iframe)
 
-    // const url = 'https://rutube.ru/api/video/' + id;
-    //
-    // fetch(url,{mode: 'no-cors'})
-    // .then((response) => {
-    //
-    //     console.log(response)
-    //     //return response.json();
-    //
-    // }).then((data) => {
-    //         console.log(data);
-    //     });
-
-
-    // const response = await fetch(url,{
-    //     mode: 'no-cors',
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    // });
-    //
-    // console.log(response)
-
-    //const apiRutube = await response.json();
-
-    //console.log(apiRutube)
-
+    return iframe;
 }
 
 videoRutubeInit('video-block')
